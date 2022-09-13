29f74b5c9fea9778d9dcea74e4c5c63c1e75756c
Jim Wharton <jamesawharton@gmail.com>
2022-09-13 11:19:55 -0600

    chore: handle errors

--- controllers/anime.js
+++ controllers/anime.js
@@ -24,15 +24,31 @@ router.get('/', (req, res) => {
 //EDIT
 
 //SHOW
-router.get('/:id', (req, res) =>
-    fetch(`https://api.jikan.moe/v4/anime/${req.params.id}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log("title", data.data.title);
-        res.render('anime/show.ejs', {
-            anime: data,
+router.get('/:id', (req, res) => {
+    const url = `https://api.jikan.moe/v4/anime/${req.params.id}`;
+    fetch(url)
+        .then((response) => {
+            // if our response from the API is NOT okay:
+            if (!response.ok) {
+                throw new Error('Failed to fetch!');
+            }
+
+            return response.json();
         })
-    })
-)
+        .then((json) => {
+console.log('json:', json);
+            res.render('anime/show.ejs', {
+                anime: json,
+                genres: json.data.genres.map(g => g.name).join(', '),
+            });
+        }).catch(err => {
+            // Handle errors by showing an error page
+            console.error('Error:', err);
+            res.render('anime/error.ejs', {
+                id: req.params.id,
+                url,
+            });
+        });
+});
 
 module.exports = router
