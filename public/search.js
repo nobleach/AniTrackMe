3ad9a02e90e76ef9f2ed0a759864d16e24b612d1
Jim Wharton <jamesawharton@gmail.com>
2022-09-14 18:32:58 -0600

    chore: get button press persisting value

---
+++ public/search.js
@@ -0,0 +1,8 @@
+const sb = document.getElementById('search-button');
+
+sb.addEventListener('click', e => {
+    e.preventDefault();
+    const searchInput = document.querySelector('#search-input').value;
+    // fetch
+});
+
